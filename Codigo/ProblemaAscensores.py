6d5807c2f9f6ed7adb88a7597df7b04e5a8e2333
Javier <javitoom96@gmail.com>
2017-06-13 11:18:29 +0200

    Clases del problema actualizadas

--- Codigo/ProblemaAscensores.py
+++ Codigo/ProblemaAscensores.py
@@ -1,19 +1,28 @@
-from jupyter_client.kernelspecapp import raw_input
-
-from interfaces import Ascensor, Persona
-
+from Codigo import Ascensor, Persona
 
 class ProblemaAscensores:
-    def __init__(self, numAscensores, numPersonas):
-        self.numAscensores = numAscensores
-        self.numPersonas = numPersonas
-        self.costeTotal = 0
+    def __init__(self):
         self.ascensores = []
         self.personas = []
-        for i in range(0,numAscensores):
-            x = Ascensor.Ascensor(raw_input('Capacidad:'),raw_input('Plantas disponibles:'),raw_input('Planta actual:'))
-            self.ascensores.append(x)
-        for i in range(0,numPersonas):
-            x = Persona.Persona(raw_input('Nombre:'),raw_input('Planta objetivo:'),raw_input('Planta actual:'))
-            self.personas.append(x)
-        
+
+    def create(self):
+        seguir = True
+        while seguir:
+            print("Creacion de ascensor")
+            capacidad = input("Capacidad: ")
+            plantasDisponibles = input("Plantas disponibles: ")
+            plantaActual = input("Planta actual: ")
+            listaPlantasDisponibles = set([int(x) for x in plantasDisponibles.strip().split(" ")])
+            velocidad = input("Velocidad (rapido/lento): ") == "rapido"
+            ascensor = Ascensor.Ascensor(capacidad,listaPlantasDisponibles,plantaActual, velocidad)
+            self.ascensores.append(ascensor)
+            seguir = input("¿Quieres crear mas ascensores?(y/n)") == "y"
+        seguir = True
+        while seguir:
+            print("Creacion de persona")
+            nombre = input("Nombre: ")
+            plantaObjetivo = input("Plantas objetivo: ")
+            plantaActual = input("Planta actual: ")
+            persona = Persona.Persona(nombre,plantaObjetivo,plantaActual)
+            self.personas.append(persona)
+            seguir = input("¿Quieres crear mas personas?(y/n)") == "y"
